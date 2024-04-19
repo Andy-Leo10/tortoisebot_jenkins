104cef6600518bd1bc03dd16d1783165d533a45d
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2024-04-19 14:40:54 +0000

    Automation1 worked manually

--- ros1_automation/check_docker.sh
+++ ros1_automation/check_docker.sh
@@ -4,9 +4,19 @@
 if ! command -v docker &> /dev/null
 then
     echo "Docker is not installed. Installing now..."
-    sudo apt-get update
-    sudo apt-get install docker.io docker-compose -y
-    sudo service docker start
+    # Check if apt-get is currently in use
+    if ps aux | grep -i apt | grep -v grep
+    then
+        echo "apt-get is currently in use. Please wait for the other process to finish before running this script again."
+    else
+        sudo apt-get update
+        sudo apt-get install docker.io docker-compose -y
+        sudo service docker start
+        sudo chmod 666 /var/run/docker.sock
+    fi
 else
     echo "Docker is already installed."
+    sudo chmod 666 /var/run/docker.sock
+    sudo apt-get update
+    sudo apt-get install x11-xserver-utils
 fi
